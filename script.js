235be92f63d6080a70fccb6e5e5af257bc447b88
Feniks7002 <07piotrr07@gmail.com>
2024-12-29 16:10:07 +0100

    Uproszczenie plikow, dodanie responsywności mobilnej

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const oneLink = document.querySelectorAll('.navbar__item');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// Zbiera wszystkie takie znaczniki (klasy,id,nav,section,itd)
+oneLink.forEach(item => {
+    item.addEventListener('click', function() {
+        menuLinks.classList.remove('active');
+        menu.classList.remove('is-active');
+    })
+})
