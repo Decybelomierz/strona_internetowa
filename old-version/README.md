2cad2f81e894d19c6d6e3ec1074e9c31020ff912
Feniks7002 <07piotrr07@gmail.com>
2025-01-05 19:08:57 +0100

    Restrukturyzacja kodu

--- old-version/README.md
+++
@@ -1,2 +0,0 @@
-# strona_internetowa
-Repozytorium utworzone do pracy nad stroną internetową Decybelomierz i materiałami, które się tam znajdą
--- old-version/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-
-// Toggle the menu on hamburger click
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
