8158d4467caf3f5d6fc0ee55bc2465e931024895
Feniks7002 <07piotrr07@gmail.com>
2025-03-02 20:16:54 +0100

    Uzupełnienie nagłówków na wszystkich sekcjach

--- Website/script/generator.js
+++ Website/script/generator.js
@@ -3,11 +3,40 @@ document.addEventListener("DOMContentLoaded", function() {
     const menu = document.querySelector('#mobile-menu');
     const menuLinks = document.querySelector('.navbar__menu');
     const oneLink = document.querySelectorAll('.navbar__item');
+    const images = document.querySelectorAll('#image');
+    const imageMAT = document.querySelectorAll('#image-mat');
+
+    images.forEach(img => {
+        const orginalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+
+        img.addEventListener('mouseenter', () => {
+            img.src = hoverSrc
+        });
+
+        img.addEventListener('mouseleave', () => {
+            img.src = orginalSrc
+        });
+    });
+    
+    imageMAT.forEach(img => {
+        const orginalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+
+        img.addEventListener('mouseenter', () => {
+            img.src = hoverSrc
+        });
+
+        img.addEventListener('mouseleave', () => {
+            img.src = orginalSrc
+        });
+    });
     
     menu.addEventListener('click', function() {
         menu.classList.toggle('is-active');
         menuLinks.classList.toggle('active');
     });
+
     // Ładowanie dźwięków
     const sounds = {
         forest: new Audio("sounds/forest.mp3"),
--- Website/script/hearing_test.js
+++ Website/script/hearing_test.js
@@ -3,6 +3,34 @@ document.addEventListener("DOMContentLoaded", function() {
     const menu = document.querySelector('#mobile-menu');
     const menuLinks = document.querySelector('.navbar__menu');
     const oneLink = document.querySelectorAll('.navbar__item');
+    const images = document.querySelectorAll('#image');
+    const imageMAT = document.querySelectorAll('#image-mat');
+
+    images.forEach(img => {
+        const orginalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+
+        img.addEventListener('mouseenter', () => {
+            img.src = hoverSrc
+        });
+
+        img.addEventListener('mouseleave', () => {
+            img.src = orginalSrc
+        });
+    });
+    
+    imageMAT.forEach(img => {
+        const orginalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+
+        img.addEventListener('mouseenter', () => {
+            img.src = hoverSrc
+        });
+
+        img.addEventListener('mouseleave', () => {
+            img.src = orginalSrc
+        });
+    });
 
     menu.addEventListener('click', function() {
         menu.classList.toggle('is-active');
