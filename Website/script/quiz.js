962e8805e6b811564129cc1af36ee8ede406d9fa
Feniks7002 <07piotrr07@gmail.com>
2025-03-05 20:51:56 +0100

    Wstępne zmiany responsywności na urządzeniach mobilnych, hamburger, wymiarowy ekran

--- Website/script/quiz.js
+++ Website/script/quiz.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Sekcja nagłówka
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
+    const oneLink = document.querySelectorAll('.navbar__item');
+
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
+    
     const images = document.querySelectorAll('#image');
     const imageMAT = document.querySelectorAll('#image-mat');
 
