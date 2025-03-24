f726e05abe8ef3c67aeb43c79b3ccb32d5249420
Feniks7002 <07piotrr07@gmail.com>
2025-03-24 22:28:14 +0100

    Podstrona konkursu

---
+++ Website/script/competition.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Sekcja nagłówkowa
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
+    const oneLink = document.querySelectorAll('.navbar__item');
+
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
+
+    // Zbiera wszystkie takie znaczniki (klasy,id,nav,section,itd)
+    oneLink.forEach(item => {
+        item.addEventListener('click', function() {
+            menuLinks.classList.remove('active');
+            menu.classList.remove('is-active');
+        });
+    });
+
+    // Sekcja przycisków
+    const images = document.querySelectorAll('#image');
+    const imageMAT = document.querySelectorAll('#image-mat');
+    images.forEach(img => {
+        const orginalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+        img.addEventListener('mouseenter', () => {
+            img.src = hoverSrc
+        });
+        img.addEventListener('mouseleave', () => {
+            img.src = orginalSrc
+        });
+    });
+
+    imageMAT.forEach(img => {
+        const orginalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+        img.addEventListener('mouseenter', () => {
+            img.src = hoverSrc
+        });
+        img.addEventListener('mouseleave', () => {
+            img.src = orginalSrc
+        });
+    });
+
+    //Pobieranie pdf przez kliknięcie
+    function downloadPDF(filePath, fileName) {
+        const link = document.createElement('a');
+        link.href = filePath;
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
+    document.getElementById('agreement').addEventListener("click", function() {
+        downloadPDF('style/download/Zgody.zip','Zgody.zip');
+    });
+});
